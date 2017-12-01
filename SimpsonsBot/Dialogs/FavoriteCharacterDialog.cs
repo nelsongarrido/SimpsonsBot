67b7d57dc0c1375ec78b44c50edc79cd7fcc50bb
nelson Garrido <ngarrido@primary.com.ar>
2017-12-01 17:00:25 -0300

    h

---
+++ SimpsonsBot/Dialogs/FavoriteCharacterDialog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpsonsBot.Dialogs
+{
+    [Serializable]
+    public class FavoriteCharacterDialog : IDialog<Model.UserProfile>
+    {
+        Model.UserProfile _profile = new Model.UserProfile();
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("What is your favorite character?");
+            context.Wait(AskForCharacter);
+        }
+
+        private async Task AskForCharacter(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var msg = await result;
+            _profile.FavoriteCharacter = msg.Text;
+            context.Done(_profile);
+        }
+    }
+}
