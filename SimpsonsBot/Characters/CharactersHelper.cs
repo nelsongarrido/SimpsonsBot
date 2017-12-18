b00e129b1adf70a64a302bceb0c3bd271354e3ee
nelson Garrido <ngarrido@primary.com.ar>
2017-12-18 14:43:21 -0300

    h

---
+++ SimpsonsBot/Characters/CharactersHelper.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpsonsBot.Question
+{
+    [Serializable]
+    public static class CharactersHelper
+    {
+        public static Model.Character GetCharacter(string character)
+        {
+            List<Model.Character> questions;
+            using (StreamReader r = new StreamReader(@"Data\characters.json"))
+            {
+                string json = r.ReadToEnd();
+                questions = JsonConvert.DeserializeObject<List<Model.Character>>(json);
+            }
+            return questions.FirstOrDefault(f => f.name == character);
+        }
+    }
+}
